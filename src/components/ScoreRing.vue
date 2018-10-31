b2f5028b853b644353a7115ed49fa35ad5a1b532
Azzinothz <30699803+Azzinothz@users.noreply.github.com>
2018-10-31 15:39:28 +0800

    feat: add ScoreRing

---
+++ src/components/ScoreRing.vue
@@ -0,0 +1,109 @@
+<template>
+    <div :style="style">
+        <v-chart :options="option" ></v-chart>
+        <button @click="test" style="width: 50px; height: 50px; font-size: 24px">🌚</button>
+    </div>
+
+</template>
+
+<script>
+import ECharts from "vue-echarts/components/ECharts"
+import "echarts/lib/chart/pie"
+
+export default {
+    components: {
+        "v-chart": ECharts
+    },
+    name: "ScoreRing",
+    data: () => {
+        let config = {
+            chartColor: "rgb(0, 150, 255)",
+            chartHeight: "400px",
+            chartWidth: "400px",
+            innerRadius: "60%",
+            outerRadius: "70%",
+            fontSize: 100,
+            textColor: "rgb(0, 0, 255)"
+        }
+
+        let data = [
+            {
+                value: 80,
+                name: "score",
+                label: {
+                    show: false
+                },
+                itemStyle: {
+                    color: config.chartColor
+                }
+            },
+            {
+                value: 20,
+                name: "blank",
+                label: {
+                    show: false
+                },
+                itemStyle: {
+                    opacity: 0
+                }
+            }
+        ]
+
+        for (let count = 0; count <= 100; count++) {
+            data.push({
+                value: 0,
+                name: count.toString(),
+                label: {
+                    show: false,
+                    position: "center",
+                    textStyle: {
+                        fontSize: config.fontSize,
+                        color: config.textColor
+                    }
+                }
+            })
+        }
+
+        data[2].label.show = true
+
+        return {
+            option: {
+                series: [
+                    {
+                        type: "pie",
+                        radius: [config.innerRadius, config.outerRadius],
+                        avoidLabelOverlap: false,
+                        labelLine: {
+                            normal: {
+                                show: false
+                            }
+                        },
+                        data: data,
+                        silent: true
+                    }
+                ]
+            },
+            style: {
+                height: config.chartHeight,
+                width: config.chartWidth
+            },
+            display: 0
+        }
+    },
+    methods: {
+        test() {
+            let self = this
+            setInterval(function() {
+                self.option.series[0].data[self.display + 2].label.show = false
+                let score = Math.round(Math.random() * 100)
+                self.option.series[0].data[0].value = score
+                self.option.series[0].data[score + 2].label.show = true
+                self.display = score
+            }, 1000)
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,42 +1,43 @@
 import Vue from "vue"
 import Router from "vue-router"
-import Login from "./views/Login"
-import Examlnit from "./views/Examlnit"
-import CandidateVerify from "./views/CandidateVerify"
-import Dashboard from "./views/Dashboard"
+// import Home from "./views/Home.vue"
+// import Login from "./views/Login"
+// import Examlnit from "./views/Examlnit"
+// import CandidateVerify from "./views/CandidateVerify"
+// import Dashboard from "./views/Dashboard"
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: "/",
-            name: "Login",
-            component: Login
-        },
-        {
-            path: "/Examlnit",
-            name: "Examlnit",
-            component: Examlnit
-        },
-        {
-            path: "/CandidateVerify",
-            name: "",
-            component: CandidateVerify
-        },
-        {
-            path: "/Dashboard",
-            name: "",
-            component: Dashboard
-        },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "about" */ "./views/About.vue")
-        }
-    ]
+    // routes: [
+    //     {
+    //         path: "/",
+    //         name: "Login",
+    //         component: Login
+    //     },
+    //     {
+    //         path: "/Examlnit",
+    //         name: "Examlnit",
+    //         component: Examlnit
+    //     },
+    //     {
+    //         path: "/CandidateVerify",
+    //         name: "",
+    //         component: CandidateVerify
+    //     },
+    //     {
+    //         path: "/Dashboard",
+    //         name: "",
+    //         component: Dashboard
+    //     },
+    //     {
+    //         path: "/about",
+    //         name: "about",
+    //         // route level code-splitting
+    //         // this generates a separate chunk (about.[hash].js) for this route
+    //         // which is lazy-loaded when the route is visited.
+    //         component: () =>
+    //             import(/* webpackChunkName: "about" */ "./views/About.vue")
+    //     }
+    // ]
 })
